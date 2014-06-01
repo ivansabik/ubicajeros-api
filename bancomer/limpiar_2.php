883a6ac996304d6f3d2f8d658e4d005f126429b6
Ivan Rodriguez <ivan.rodriguez.o@gmail.com>
2014-06-01 16:11:36 -0500

    faltan 3 y 4

--- bancomer/limpiar_2.php
+++ bancomer/limpiar_2.php
@@ -6,34 +6,29 @@ $sucursales = json_decode($json_sucursales);
 $scrapped_sucursales = $sucursales->d;
 # var_dump($scrapped_sucursales);
 $sucursales = array();
-        echo '<pre>';
-        print_r($scrapped_sucursales);
-        echo '</pre>';
-/*
 foreach($scrapped_sucursales as $scrappped_sucursal) {
 	$sucursal = array();
 	$sucursal['nombre'] = $scrappped_sucursal->Nombre;
 	$sucursal['lat'] = $scrappped_sucursal->Latitud;
 	$sucursal['lon'] = $scrappped_sucursal->Longitud;
 	$campos = $scrappped_sucursal->Campos;
-	$sucursal['categoria'] = $campos[3]->Valor;
-	$sucursal['calle'] = $campos[6]->Valor;
+	$sucursal['domicilio'] = $campos[6]->Valor;
+	$sucursal['categoria'] = $campos[4]->Valor;
 	$sucursal['calle'] = $campos[7]->Valor;
-	$sucursal['num_ext'] = $campos[7]->Valor;
-	$sucursal['colonia'] = $campos[8]->Valor;
+	$sucursal['num_ext'] = $campos[8]->Valor;
+	$sucursal['colonia'] = $campos[35]->Valor;
 	$sucursal['cp'] = $campos[9]->Valor;
 	$sucursal['ciudad'] = $campos[12]->Valor;
-	$sucursal['estado'] = $campos[13]->Valor;
-	$sucursal['id_estado'] = $campos[28]->Valor;
-	$sucursal['id_municipio'] = $campos[29]->Valor;
-	$sucursal['id_localidad'] = $campos[30]->Valor;
-	$sucursal['id_colonia'] = $campos[31]->Valor;
+	$sucursal['estado'] = $campos[28]->Valor;
+	$sucursal['id_estado'] = $campos[27]->Valor;
+	$sucursal['id_municipio'] = $campos[30]->Valor;
+	$sucursal['id_localidad'] = $campos[32]->Valor;
+	$sucursal['id_colonia'] = $campos[34]->Valor;
 	$sucursales[] = $sucursal;
 }
-file_put_contents('1_.json', json_encode($sucursales));
+file_put_contents('2_.json', json_encode($sucursales));
 
         echo '<pre>';
         var_dump($sucursales);
         echo '</pre>';
-*/
 ?>
